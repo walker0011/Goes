ea13f136a9e92398be291fa9d31054cf0509f25d
walker0011 <1515037502@qq.com>
2021-12-15 10:54:42 +0800

    using channels

--- 12-concurrency.go
+++ 12-concurrency.go
@@ -2,17 +2,33 @@ package main
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
-
+	resultChannel := make(chan result)
 	for _, url := range urls {
 		// 匿名函数的好处：
 		//  1. 可以在声明的时候同时执行
 		//  2. 所有在声明匿名函数出可用的变量在匿名函数体内都可用
 
-		go func() {
-			results[url] = wc(url)
-		}()
+		// go func() {
+		// 	// 注意这种方式：每个goroutine都是url的reference，因此最后写入的是最后一个url信息
+		// 	results[url] = wc(url)
+		// }()
+
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
 	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
 	return results
 }
