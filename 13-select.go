91e5b0575bb998f8fc6ec1479971ae1a5a8648c7
walker0011 <1515037502@qq.com>
2021-12-15 21:18:57 +0800

    first refactor

---
+++ 13-select.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+// a b 分别是URL地址
+func Racer(a, b string) (winner string) {
+
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
+
+	if aDuration < bDuration {
+		return a
+	}
+	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
---
+++ 13-select_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	// by prefixing a function with "defer", it will call this function
+	// at the end of the containint function
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				time.Sleep(delay)
+				w.WriteHeader(http.StatusOK)
+			}))
+}
